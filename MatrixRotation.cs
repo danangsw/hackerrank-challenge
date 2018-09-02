6317ca69cd58a65f32b2c2fc66ceb52e26462361
Danang Sukma <danangsukma@gmail.com>
2018-09-02 17:36:19 +0700

    Update MatrixRotation.cs
    100% UT completed

--- MatrixRotation.cs
+++ MatrixRotation.cs
@@ -31,30 +31,46 @@ using System.Text.RegularExpressions;
 using System.Text;
 using System;
 
-class MatrixRotation {
+class Solution {
 
     // Complete the matrixRotation function below.
     static void matrixRotation(List<List<int>> matrix, int r) {
         int mz = matrix.Count();
         int nz = matrix.First().Count();
         int maxSize = mz * nz;
+        int [,] newMatrix = new int[mz,nz];
         List<List<int>> matrixEvo = new List<List<int>>();
+        List<List<int>> matrixRotated = new List<List<int>>();
+        List<List<int[]>> matrixCoordinate = new List<List<int[]>>();
         
         int cls = -1;
         List<int> matrixLayer = new List<int>();
+        List<int[]> matrixCoorItem = new List<int[]>();
         int x = cls;
         int y = cls;
         mz--;
         nz--;
         for(int i = 0; i<maxSize; i++) {
             if(x == cls && y == cls) {
+                // Add the list item
+                if(cls >= 0) {
+                    matrixEvo.Add(matrixLayer);
+                    matrixCoordinate.Add(matrixCoorItem);
+                }
                 cls++;
                 x = cls;
                 y = cls;
-                Console.WriteLine("--{0}--", cls);
+                matrixLayer = new List<int>();
+                matrixCoorItem = new List<int[]>();
+                //Console.WriteLine("--{0}--", cls);
             }
             
-            Console.WriteLine("{0}, {1}", x, y);
+            if(cls >= 0) {
+                matrixLayer.Add(matrix[x][y]);
+                matrixCoorItem.Add(new int[2] {x, y});
+            }
+            
+            // Console.WriteLine("{0}, {1} --> {2}", x, y, matrix[x][y]);
             
             // left side
             if(x < (mz-cls) && y == cls) {
@@ -71,9 +87,62 @@ class MatrixRotation {
             // top side
             else if(y > cls && x == cls) {
                 y--;
-            }
-            
+            }   
         }
+        // Add the latest list item
+        if(cls >= 0) {
+            matrixEvo.Add(matrixLayer);
+            matrixCoordinate.Add(matrixCoorItem);
+        }
+        
+        // Rotate the list item by r rotate/s
+        foreach(var me in matrixEvo) {
+            int cme = me.Count;
+            int rot = r%cme;
+            int[] newArr = new int[cme];
+            
+            int i = 0;
+            foreach(var ime in me) {
+               //Console.Write("{0} ", i);
+                int newIdx = (i+rot) % cme ;
+                newArr[newIdx] = ime;
+                i++;
+            }
+            //Console.WriteLine(cme);
+            List<int> insArr = new List<int>();
+            insArr.AddRange(newArr);
+            matrixRotated.Add(insArr);
+        }
+        
+        // Convert the list item to matrix array
+        int idx = 0;
+        foreach(var me in matrixRotated) {
+            var coors = matrixCoordinate[idx];
+            int cme = me.Count;
+            r = r%cme;
+            int[] newArr = new int[cme];
+            
+            int i = 0;
+            foreach(var ime in me) {
+                var coor = coors[i];
+                // Console.WriteLine("{0},{1} --> {2}", coor[0], coor[1], ime);
+                newMatrix[coor[0], coor[1]] = ime;
+               i++;
+            }
+
+            idx++;
+            // Console.WriteLine(cme);
+        }
+        
+        // PRINT RESULT
+        mz++;
+        nz++;
+        for(int i = 0; i < mz; i++) {
+            for(int j = 0; j < nz; j++) {
+                Console.Write("{0} ", newMatrix[i,j]);
+            }
+            Console.WriteLine();
+        } 
     }
     
     static void Main(string[] args) {
